445291722637752dfe181c3b2d2c39354d89f671
yuanhuihai <yuanhuihai@sina.com>
2023-05-24 16:38:26 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace ABBSecond
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.PX1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.RZ1 = new System.Windows.Forms.TextBox();
+            this.PY1 = new System.Windows.Forms.TextBox();
+            this.RY1 = new System.Windows.Forms.TextBox();
+            this.PZ1 = new System.Windows.Forms.TextBox();
+            this.RX1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.PX1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.RZ1);
+            this.groupBox1.Controls.Add(this.PY1);
+            this.groupBox1.Controls.Add(this.RY1);
+            this.groupBox1.Controls.Add(this.PZ1);
+            this.groupBox1.Controls.Add(this.RX1);
+            this.groupBox1.Location = new System.Drawing.Point(37, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(290, 162);
+            this.groupBox1.TabIndex = 23;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "坐标位置";
+            // 
+            // PX1
+            // 
+            this.PX1.Location = new System.Drawing.Point(96, 68);
+            this.PX1.Name = "PX1";
+            this.PX1.Size = new System.Drawing.Size(35, 21);
+            this.PX1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(151, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(151, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(151, 127);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 12);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "label7";
+            // 
+            // RZ1
+            // 
+            this.RZ1.Location = new System.Drawing.Point(210, 122);
+            this.RZ1.Name = "RZ1";
+            this.RZ1.Size = new System.Drawing.Size(35, 21);
+            this.RZ1.TabIndex = 14;
+            // 
+            // PY1
+            // 
+            this.PY1.Location = new System.Drawing.Point(96, 95);
+            this.PY1.Name = "PY1";
+            this.PY1.Size = new System.Drawing.Size(35, 21);
+            this.PY1.TabIndex = 10;
+            // 
+            // RY1
+            // 
+            this.RY1.Location = new System.Drawing.Point(210, 95);
+            this.RY1.Name = "RY1";
+            this.RY1.Size = new System.Drawing.Size(35, 21);
+            this.RY1.TabIndex = 13;
+            // 
+            // PZ1
+            // 
+            this.PZ1.Location = new System.Drawing.Point(96, 122);
+            this.PZ1.Name = "PZ1";
+            this.PZ1.Size = new System.Drawing.Size(35, 21);
+            this.PZ1.TabIndex = 11;
+            // 
+            // RX1
+            // 
+            this.RX1.Location = new System.Drawing.Point(210, 68);
+            this.RX1.Name = "RX1";
+            this.RX1.Size = new System.Drawing.Size(35, 21);
+            this.RX1.TabIndex = 12;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 150;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox PX1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox RZ1;
+        private System.Windows.Forms.TextBox PY1;
+        private System.Windows.Forms.TextBox RY1;
+        private System.Windows.Forms.TextBox PZ1;
+        private System.Windows.Forms.TextBox RX1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using ABB.Robotics.Controllers;
+using ABB.Robotics.Controllers.Discovery;
+using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace ABBSecond
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        static Controller controller;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            NetworkScanner scanner = new NetworkScanner();        
+            scanner.Scan();
+            ControllerInfoCollection infos = scanner.Controllers;      
+            controller = new Controller(infos[0]);
+            controller.Logon(UserInfo.DefaultUser);
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ABB.Robotics.Controllers.RapidDomain.JointTarget RJ = controller.MotionSystem.ActiveMechanicalUnit.GetPosition();
+            label2.Text = "J1:";
+            label3.Text = "J2:";
+            label4.Text = "J3:";
+            label5.Text = "J4:";
+            label6.Text = "J5:";
+            label7.Text = "J6:";
+            PX1.Text = RJ.RobAx.Rax_1.ToString(format: "0.00");
+            PY1.Text = RJ.RobAx.Rax_2.ToString(format: "0.00");
+            PZ1.Text = RJ.RobAx.Rax_3.ToString(format: "0.00");
+            RX1.Text = RJ.RobAx.Rax_4.ToString(format: "0.00");
+            RY1.Text = RJ.RobAx.Rax_5.ToString(format: "0.00");
+            RZ1.Text = RJ.RobAx.Rax_6.ToString(format: "0.00");
+
+
+        // string path = @"C:\Users\yuan\My project\Assets\streamingAssets\123.sqlite"; //本地路径
+
+        string path = @"C:\Users\yuan\Desktop\test\My project_Data\StreamingAssets\123.sqlite";  //项目发布后，数据库路径 20230523
+         SQLiteConnection cn = new SQLiteConnection("data source=" + path);
+            cn.Open();
+            string sql = "update abb set one= " + PX1.Text + ",two= " + PY1.Text + ",three= " + PZ1.Text + ",four= " + RX1.Text + ",five= " + RY1.Text + ",six= " + RZ1.Text + " where xuhao=1";
+            SQLiteCommand command = new SQLiteCommand(sql, cn);
+            command.ExecuteNonQuery();
+            cn.Close();
+
+
+
+
+        }
+    }
+}
+
